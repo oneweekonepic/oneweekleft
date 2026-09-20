9b589ef60bd86a846eae5a35adb0a0f62f320cc5
Karim Hadjeba <karim.hadjeba@gmail.com>
2026-09-20 20:05:34 +0200

    Add Amazon Music TIDAL and Deezer links

--- app/src/data/releases.ts
+++ app/src/data/releases.ts
@@ -8,5 +8,8 @@ export const featuredRelease = {
     spotify: 'https://open.spotify.com/artist/2bZK1W2n7BljOwKGAXpMxl',
     appleMusic: 'https://music.apple.com/ch/artist/one-week-left/6802715735',
     youtube: 'https://www.youtube.com/channel/UCY3Gk5U7LYMpTQTTK2B4lkA',
+    amazonMusic: 'https://music.amazon.fr/artists/B0HFPVWPWN/one-week-left',
+    tidal: 'https://tidal.com/artist/84609716',
+    deezer: 'https://link.deezer.com/s/34sdXTiGMOGXX6RrG8mqn',
   },
 } as const;
